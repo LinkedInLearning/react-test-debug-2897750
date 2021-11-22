45f21ffb5a4d1256a224331069c355d96182d612
Sandy Instructor <sandyludosky@gmail.com>
2021-11-22 06:25:57 -0800

    FR_2897750_02_04_beginning

---
+++ FR_2897750_02/FR_2897750_02_04/src/App.js
@@ -0,0 +1,63 @@
+import { useState, useMemo, useRef } from "react";
+import './App.css';
+
+
+export const TrashButton = ({ isVisible, remove }) => isVisible ? <button onClick={remove}>remove completed</button> : null
+
+export const TodoList = ({ items, onClick }) => (
+<ul className="mt-3">
+{items.map((item) => (
+  <li key={item.id} className={`${item.done && "done"}`} onClick={() => onClick(item.id)}>
+    {item.value}
+  </li>
+))}
+</ul>)
+
+function App() {
+  const inputRef = useRef()
+  const [value, setValue] = useState(null)
+  const [items, setItems] = useState([]);
+  
+  const uid = () => Math.floor(Math.random() * 900);
+  const handleOnChange = (e) => setValue(e.target.value)
+  const handleOnClick = (id) => {
+    const updated = items.map(item => {
+      return item.id === id ? { ...item, done: !item.done } : item
+    })
+    setItems(updated)
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    if (!value) { return false }
+    setItems([...items, { id: uid(), value, done: false }])
+    inputRef.current.value = ""
+    setValue(null)
+  }
+  const remove = () => {
+    const filtered = items.filter(item => !item.done)
+    setItems(filtered)
+  }
+ 
+  const hasTodoDone = useMemo(() => {
+    return items.find(item => item.done) 
+  }, [items])
+
+  return (
+    <div className="App">
+        <h1 className="mb-2">La Todo Liste</h1>
+        <form data-testid="form" onSubmit={handleOnSubmit} style={{ display: "flex" }}>
+          <input
+            ref={inputRef}
+            className="form-control"
+            type="text"
+            name="inputTodo"
+            onChange={handleOnChange}
+          />
+        </form>
+        <TodoList items={items} onClick={handleOnClick} />
+        <TrashButton isVisible={hasTodoDone} remove={remove} />
+    </div>
+  );
+}
+
+export default App;
