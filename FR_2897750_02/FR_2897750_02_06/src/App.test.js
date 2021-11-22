4f5be414a9bed9cffc522f444dfeb02339a90cdf
Sandy Instructor <sandyludosky@gmail.com>
2021-11-22 13:32:46 -0800

    FR_2897750_02_06_end

--- FR_2897750_02/FR_2897750_02_06/src/App.test.js
+++ FR_2897750_02/FR_2897750_02_06/src/App.test.js
@@ -3,4 +3,3 @@ import { useReducer } from 'react'
 import { renderHook, act } from '@testing-library/react-hooks'
 import { ADD_ITEM, CHECK_ITEM, REMOVE_ITEMS } from './context'
 import reducer, { initialState }  from "./context/reducer";
-
