5a05677d915012a4469f876a8a7f7ed719c0a680
Sandy Instructor <sandyludosky@gmail.com>
2021-11-22 04:25:37 -0800

    FR_2897750_02_02_end

--- FR_2897750_02/FR_2897750_02_02/src/App.test.js
+++ FR_2897750_02/FR_2897750_02_02/src/App.test.js
@@ -1,3 +1,10 @@
+import { render, screen} from "@testing-library/react"
+import App from './App'
 
+test('renders learn react link', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/Learn React Testing Library/i)
+    expect(linkElement).toBeInTheDocument()
+})
 
 
