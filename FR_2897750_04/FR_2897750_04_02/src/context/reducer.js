72e0eec8edba701fbfffdf11519563b28316b0e4
Sandy Instructor <sandyludosky@gmail.com>
2021-11-23 06:38:40 -0800

    FR_2897750_04_02_beginning

---
+++ FR_2897750_04/FR_2897750_04_02/src/context/reducer.js
@@ -0,0 +1,46 @@
+
+const ADD_ITEM = "ADD_ITEM";
+const CHECK_ITEM = "CHECK_ITEM";
+const REMOVE_ITEMS = "REMOVE_ITEMS";
+
+
+export const initialState = {
+  items: [
+    { id: 123, value: "item #1", done: false },
+    { id: 124, value: "item #2", done: false },
+  ],
+};
+
+function uid() {
+  return Math.floor(Math.random() * 900);
+}
+function reducer(state = initialState, { type, payload }) {
+  function check(item) {
+    return item.id === payload.id
+      ? { ...item, done: !item.done }
+      : item;
+  }
+  function remove(item) { return !item.done}
+  switch (type) {
+    case ADD_ITEM:
+      debugger;
+      return {
+        items: [...state.items, {id: uid(), value: payload.value, done: false}],
+      };
+
+    case CHECK_ITEM:
+      
+      return {
+        items: state.items.map(check),
+      };
+    case REMOVE_ITEMS:
+
+      return {
+        items: state.items.filter(remove),
+      };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
---
+++ FR_2897750_04/FR_2897750_04_02/src/context/types.js
@@ -0,0 +1,3 @@
+export const ADD_ITEM = "ADD_ITEM";
+export const CHECK_ITEM = "CHECK_ITEM";
+export const REMOVE_ITEMS = "REMOVE_ITEMS";
